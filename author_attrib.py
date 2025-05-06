820ab921ebb5a8a326794b285573986046652583
AidanS39 <aidan.stoner39@gmail.com>
2025-05-06 14:17:36 -0400

    refactored t3 a bit

--- author_attrib.py
+++ author_attrib.py
@@ -13,17 +13,17 @@ def generate_test_train_numbers(num_range: int, test_ratio: float):
            }
 
 def generate_all_author_files(numbers: dict, tsv_file_path: str, train_file_path: str, test_file_path: str):
-    authors = set()
+    authors = dict()
     vocab = set()
     with open(tsv_file_path, "r") as tsv_file:
         all_reviews = tsv_file.readlines() 
         current_author = all_reviews[0].split('\t')[1]
         current_reviews = list()
+        
         for review in all_reviews:
             review = review.split('\t')
             if current_author != review[1]:
-                generate_author_files(numbers, current_reviews, current_author, train_file_path, test_file_path)
-                authors.add(current_author)
+                authors[current_author] = generate_author_files(numbers, current_reviews, current_author, train_file_path, test_file_path)
                 current_reviews.clear()
                 current_author = review[1]
             try:
@@ -32,12 +32,12 @@ def generate_all_author_files(numbers: dict, tsv_file_path: str, train_file_path
             except:
                 print("Error occured: text section does not exist")
                         
-        generate_author_files(numbers, current_reviews, current_author, train_file_path, test_file_path)
-        authors.add(current_author)
+        authors[current_author] = generate_author_files(numbers, current_reviews, current_author, train_file_path, test_file_path)
     return authors, vocab         
   
               
 def generate_author_files(numbers: dict, reviews: str, author: str, train_path: str, test_path: str):
+    train_reviews = list()
     with open(f"{train_path}/{author}.txt", "w") as train_file:
         with open(f"{test_path}/{author}.txt", "w") as test_file:
             for index, review in enumerate(reviews):
@@ -45,30 +45,35 @@ def generate_author_files(numbers: dict, reviews: str, author: str, train_path: 
                     test_file.write(review)
                 else:
                     train_file.write(review)
+                    train_reviews.append(review)
+    return train_reviews
+                    
 
 
 
-def generate_all_unigrams(authors: set(), vocab: set(), train_path: str):
+def generate_all_unigrams(authors: dict(), vocab: set(), train_path: str):
     unigram_counts = {author: {word: 0 for word in vocab} for author in authors}
     unigrams = {author: {word: 0 for word in vocab} for author in authors}
     vocab_length = len(vocab)
     i = 0
     for author in authors:
-        with open(f"{train_path}/{author}.txt", "r") as author_file:
-            reviews = author_file.readlines()
-            reviews_length = 0
-            for review in reviews:
-                i += 1
-                split_review = review.split()
-                reviews_length += len(split_review)
-                for word in split_review:
-                    unigram_counts[author][word] += 1
-            for word in unigram_counts[author]:
-                unigrams[author][word] = (unigram_counts[author][word] + 1)/(reviews_length + vocab_length)
-    
+        reviews = authors[author]
+        reviews_length = sum(len(review.split()) for review in reviews)
+        for review in reviews:
+            i += 1
+            review = review.split()
+            for word in review:
+                unigram_counts[author][word] += 1
+        for word in unigram_counts[author]:
+            unigrams[author][word] = (unigram_counts[author][word] + 1)/(reviews_length + vocab_length)
+        if author is list(authors)[0]:
+            print(unigram_counts[author])
+            print(author)    
     print(len(unigrams))
     print(i)        
     return unigrams 
+
+
 
 numbers = generate_test_train_numbers(1000, 0.10)
 authors, vocab = generate_all_author_files(numbers, "./imdb62.tsv", "./train", "./test")
